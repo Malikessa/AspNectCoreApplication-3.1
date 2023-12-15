a3a09de91931c3d9bfae1ae1804502b8191fd84a
malikessa <essakhan7868@gmail.com>
2023-12-16 00:02:59 +0500

    Entire Project Has been pushed

---
+++ EmployeeManagementSample02/EmployeeManagementSample02/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSample02.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statuscode}")]
+        public IActionResult HttpExceptionHandler(int statuscode)
+        {
+            switch (statuscode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "The requested URL can not be found";
+                    break;
+            }
+            return View("NotFound");
+        }
+        [Route("Error")]
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.ExceptionPath = exceptionDetails.Path;
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
+
+            return View("Error");
+        }
+    }
+}
---
+++ EmployeeManagementSample02/EmployeeManagementSample02/Migrations/20231215184506_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagementSample02.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Department = table.Column<int>(type: "int", nullable: false),
+                    PhotoPath = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name", "PhotoPath" },
+                values: new object[] { 1, 2, "mark@pragimtech.com", "Mark", null });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name", "PhotoPath" },
+                values: new object[] { 2, 1, "smith@pragimtech.com", "Smith", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
